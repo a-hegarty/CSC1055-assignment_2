74edc9f0361862ae9e03eef76fa4012c0100fed9
A.Hegarty <a.hegarty004@gmail.com>
2025-11-11 23:01:01 +0000

    implemented state data type

--- finnite_State_Automata.hs
+++ finnite_State_Automata.hs
@@ -1,5 +1,8 @@
 --making finite state automata
-type Fin_State = (Int, Int, Int)
+-- each state in finite state automata will take the form Node startstate transition endstate
+-- e.g. Node 0 a 1 <- going from node 0 to node 1 via transition a 
+data Fin_State = Empty | Node Int Char Int
+    deriving(Show, Eq, Ord)
 
 -- addState
 
