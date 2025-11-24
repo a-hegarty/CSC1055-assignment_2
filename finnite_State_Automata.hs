1e7f86b3f449611177ef729352e42c6e39a584e5
A.Hegarty <a.hegarty004@gmail.com>
2025-11-24 15:09:10 +0000

    added types for Ndfa & dfa

--- finnite_State_Automata.hs
+++ finnite_State_Automata.hs
@@ -1,8 +1,7 @@
 --making finite state automata
--- each state in finite state automata will take the form Node startstate transition endstate
--- e.g. Node 0 a 1 <- going from node 0 to node 1 via transition a 
-data Fin_State = Empty | Node Int Char Int
-    deriving(Show, Eq, Ord)
+type NDFA = ([Integer], Integer, [Integer], (Integer->Char->[Integer]))
+
+type DFA = ([Integer], Integer, [Integer], (Integer->Char->Integer))
 
 -- addState
 
